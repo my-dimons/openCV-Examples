327a93bfaff5c422d433d66621523a8cb410de04
mydimons <mydimons.kb@gmail.com>
2026-02-23 15:04:25 -0800

    webcam working

--- read.py
+++ read.py
@@ -5,15 +5,13 @@ cam = cv.VideoCapture(0);
 frame_width = int(cam.get(cv.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cam.get(cv.CAP_PROP_FRAME_HEIGHT))
 
-#fourcc = cv.VideoWriter_fourcc(*'mp4v')
-#out = cv.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+while True:
+    ret, frame = cam.read();
 
+    cv.imshow('webcam', frame)
 
-ret, frame = cam.read();
+    if cv.waitKey(1) == ord('q'):
+        break
 
-#out.write(frame)
-
-cv.imshow('webcam', frame)
-
-# When the '0' key is pressed, close the window
-cv.waitKey(0);
+cam.release()
+cv.destroyAllWindows()
