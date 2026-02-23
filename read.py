a38780c715c5f165e55fd298c226fc988f9852ed
mydimons <mydimons.kb@gmail.com>
2026-02-23 15:52:07 -0800

    frame resizing

--- read.py
+++ read.py
@@ -6,5 +6,12 @@ img = cv.imread('photos/cat_large.jpg')
 # Open window
 cv.imshow('Image recognition', img)
 
+def rescaleFrame(frame, scale = 0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
 # When any key is pressed, close the window
 cv.waitKey(0);
