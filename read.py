ad8f8e0708500198a50765a31ffc462561fd971e
mydimons <mydimons.kb@gmail.com>
2026-02-23 15:48:44 -0800

    set FPS

--- read.py
+++ read.py
@@ -1,12 +1,16 @@
 import cv2 as cv
+import time
 
-capture = cv.VideoCapture('videos/dog.mp4')
+capture = cv.VideoCapture('videos/kitten.mp4')
+
+FPS = 1 / 30
 
 while True:
     isTrue, frame = capture.read()
 
     cv.imshow('video', frame)
 
+    time.sleep(FPS)
     if cv.waitKey(1) == ord('q'):
         break
 
