e502da4854e24b15980ad1075eb1ca1c5be51b97
mydimons <mydimons.kb@gmail.com>
2026-02-23 15:00:17 -0800

    camera frame
    getting a single camera frame on start

--- read.py
+++ read.py
@@ -1,10 +1,19 @@
 import cv2 as cv
 
-# Read image
-img = cv.imread('photos/cat.jpg')
+cam = cv.VideoCapture(0);
 
-# Open window
-cv.imshow('Image recognition', img)
+frame_width = int(cam.get(cv.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cam.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+#fourcc = cv.VideoWriter_fourcc(*'mp4v')
+#out = cv.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+
+
+ret, frame = cam.read();
+
+#out.write(frame)
+
+cv.imshow('webcam', frame)
 
 # When the '0' key is pressed, close the window
 cv.waitKey(0);
