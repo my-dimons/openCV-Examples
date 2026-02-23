8b9e0d089aaeb2f2cb78184588c372d4e4b40c52
mydimons <mydimons.kb@gmail.com>
2026-02-23 15:56:45 -0800

    many kitties

--- read.py
+++ read.py
@@ -5,12 +5,26 @@ capture = cv.VideoCapture('videos/kitten.mp4')
 
 FPS = 1 / 30
 
+def rescaleFrame(frame, scale = 0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
 while True:
     isTrue, frame = capture.read()
 
-    cv.imshow('video', frame)
+    if isTrue == False:
+        break
+
+    frame_resized = rescaleFrame(frame, 0.1)
+    
+    for i in range(0, 60):
+        cv.imshow('video ' + str(i), frame_resized)
 
     time.sleep(FPS)
+
     if cv.waitKey(1) == ord('q'):
         break
 
